d4ffe4efd5a2cbd44ba0bfdcf3d31c32f03fbe70
ramadearbol <ramon.gavira@a.vedrunasevillasj.es>
2025-04-30 13:12:41 +0200

    Aprender Creacion

--- frontend/src/pages/Aprender.jsx
+++ frontend/src/pages/Aprender.jsx
@@ -1,1 +1,14 @@
+import "../styles/Aprender.css";
 
+// src/pages/Aprender.jsx
+
+function Aprender() {
+    return (
+      <div>
+        <h1>Página de Aprender</h1>
+      </div>
+    );
+  }
+  
+  export default Aprender;
+  
