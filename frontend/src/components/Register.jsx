0fd69ee52c8b4a1e0e764e6ff68e28f4be1f0dfa
ramadearbol <ramon.gavira@a.vedrunasevillasj.es>
2025-05-12 13:21:11 +0200

    Enlace Tienes Cuenta

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -10,6 +10,10 @@ function Register() {
     navigate("/home/aprender");
   };
 
+  const goToLogin = () => {
+    navigate("/");
+  }
+
   return (
     <div className="register-container">
       <div className="login-box">
@@ -21,6 +25,7 @@ function Register() {
           <input type="text" placeholder="Usuario" className="input-field" />
           <input type="password" placeholder="Contraseña" className="input-field" />
           <button className="register-btn" onClick={handleRegister}>Finalizar Registro</button>
+          <p className="nav-link" onClick={goToLogin}>¿Tienes cuenta? Inicia Sesión</p>
         </div>
       </div>
     </div>
