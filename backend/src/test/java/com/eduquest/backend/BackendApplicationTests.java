9203317f2d0abb5410559ebca391c74c4e226b27
ramadearbol <ramon.gavira@a.vedrunasevillasj.es>
2025-05-13 10:41:48 +0200

    Inicio Spring boot

---
+++ backend/src/test/java/com/eduquest/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eduquest.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
+
+
+@Disabled("Disabled until Firebase is integrated")
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
