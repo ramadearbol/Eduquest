98a4643186bb0e795a795e3743b5b977171ad525
ramadearbol <ramon.gavira@a.vedrunasevillasj.es>
2025-06-05 16:56:46 +0200

    Prueba Nueva Raylway

--- backend/src/main/java/com/eduquest/backend/BackendApplication.java
+++ backend/src/main/java/com/eduquest/backend/BackendApplication.java
@@ -2,10 +2,14 @@ package com.eduquest.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.eduquest.backend.model")
+@EnableJpaRepositories(basePackages = "com.eduquest.backend.repository")
 @EnableScheduling
 public class BackendApplication {
     public static void main(String[] args) {
